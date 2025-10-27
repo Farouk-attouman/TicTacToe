fa2070c5f0c56f71a6b30cfa0e0485dd093e88a8
faOum <attouman.farouk@gmail.com>
2025-10-27 21:15:24 +0100

    creation of display_board a function that create the game board

---
+++ main.py
@@ -0,0 +1,9 @@
+def display_board(board):
+    print("\n")
+    print(f" {board[0]} | {board[1]} | {board[2]}")
+    print(f"---|---|---")
+    print(f" {board[3]} | {board[4]} | {board[5]}")
+    print(f"---|---|---")
+    print(f" {board[6]} | {board[7]} | {board[8]}")
+    print("\n")
+
