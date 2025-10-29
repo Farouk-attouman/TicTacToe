bfd79e673936085168dd014838178d0004b0e4cf
faOum <attouman.farouk@gmail.com>
2025-10-29 13:55:42 +0100

    creation of player_vs_ai a function that permit to play against vs computer

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def ai_hard(board, symbol):
 
     # Jouer au centre
     if board[4] == " ":
-        return board[4]
+        return 4
     
     # Jouer les coins
     corners = [0,2,6,8]
@@ -160,10 +160,119 @@ def ai_hard(board, symbol):
     for middle in middles :
         if middle in empty_case :
             return middle
+        
 
+def player_vs_ai():
+    board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    print("\n----JOUEUR VS IA----")
+    print("\nChoisir le niveau de l'IA")
+    print("1. Mode Facile")
+    print("2. Mode Difficile")
 
+    while True:
+        try:
+            choice = int(input("Entrer votre choix 1-2 : "))
+            if choice == 1:
+                ai_function = ai_easy
+                level_ai = "facile"
+                break
 
+            elif choice == 2:
+                ai_function = ai_hard
+                level_ai = "difficile"
+                break
 
+            else: 
+                print("Veuillez entrer soit 1 ou 2 : ")
+
+        except ValueError:
+            print("Veuillez entre un chiffre")
+    
+    print("\nQui joue en premier")
+    print("1. Joueur")
+    print("2. IA")
+
+    while True:
+        try: 
+            first_player = int(input("entrer votre choix 1-2 : "))
+
+            if first_player == 1:
+                player_symbol = "X"
+                ai_symbol = "O"
+                player_turn = True
+                break
+
+            elif first_player == 2:
+                player_symbol = "O"
+                ai_symbol = "X"
+                player_turn = False
+                break
+
+            else: 
+                print("Veuillez entrer soit 1 soit 2")
+        except ValueError:
+            print("Veuillez entrer un chiffre")
+        
+    print(f"Vous jouez avec {player_symbol}")
+    print(f"l'IA joue avec {ai_symbol}")
+    game_command()
+
+    while True:
+        display_board(board)
+
+        if player_turn:
+            # TOUR DU JOUEUR
+            print(f"Tour du joueur {player_symbol}")
+            while True:
+                try:
+                    player_choice = int(input("Entrer un chiffre entre 1-9 : "))
+                    position = player_choice - 1
+
+                    if position < 0 or position > 8:
+                        print("Veuillez entrer un chiffre entre 1 et 9 : ")
+                        continue
+
+                    if board[position] != " ":
+                        print("Cette case est déjà occupée")
+                        continue
+
+                    break
+                except ValueError:
+                    print("Veuillez entrez un chiffre svp")
+
+            board[position] = player_symbol
+            current_symbol = player_symbol
+            
+        else:
+            # TOUR DE L'IA
+            print(f"Tour de l'IA {level_ai} ")
+            position_ai = ai_function(board, ai_symbol)
+            
+            if position_ai is not False:
+                board[position_ai] = ai_symbol
+            else:
+                print("Erreur de l'IA")
+                break
+                
+            current_symbol = ai_symbol
+
+        # Vérifier la VICTORE
+        if check_victory(board, current_symbol):
+            display_board(board)
+            if current_symbol == player_symbol:
+                print(f"Bravo, vous avez gagné contre l'IA {level_ai} !")
+            else:
+                print(f"L'IA {level_ai} a gagné !")
+            break
+
+        # Vérifier MATCH NUL
+        if board_full(board):
+            display_board(board)
+            print("🤝 Match nul !")
+            break
+
+        # Changer de tour
+        player_turn = not player_turn
 
 
 def main_menu():
@@ -171,27 +280,33 @@ def main_menu():
         print(f"\nMenu Principal du jeu")
         print(f"\n=== Jeu du TicTacToe ===")
         print(f"1. Joueur vs Joueur")
-        print(f"2. Quittez")
+        print(f"2. Joueur vs IA")
+        print(f"3. Quittez")
 
         choice = int(input(f"\nQuel mode de jeu voulez-vous jouer ? "))
         
         if choice == 1 :
             player_vs_player()
-
-            replay = input("Voulez-vous rejouer ? (o/n) :")
-            if replay != "o":
-                print("Merci d'avoir joué !")
-                break
-
+        
         elif choice == 2 :
+            player_vs_ai()
+
+        elif choice == 3 :
             print("Merci à bientôt")
             break
 
         else :
             print("Veuillez entrer une option valide")
+            continue
+
+        replay = input("Voulez-vous rejouer ? (o/n) :")
+        if replay.lower() != "o":
+            print("Merci d'avoir joué !")
+            break
 
 
 
 main_menu()
 
 
+    
